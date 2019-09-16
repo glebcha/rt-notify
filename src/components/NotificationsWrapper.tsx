341ffe0c4391676dc01786eff5f5236393df011b
Gleb Popov <gpopov@gotrg.com>
2019-09-16 18:53:39 +0700

    refactor: added types

--- src/components/NotificationsWrapper.tsx
+++ src/components/NotificationsWrapper.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const NotificationsWrapper = styled.div`
+export const NotificationsWrapper = styled.div<{placement: string}>`
     position: fixed;
     z-index: 999999;
     max-width: 80%;
