544de02e5e96afe38d2a981605c8a0459c169d82
Gleb Popov <glebcha@gmail.com>
2020-02-03 14:25:53 +0700

    feat(styles): notification position animation mixin

---
+++ src/styles/mixins/notification-position.js
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+function getCssTransform(placement) {
+  const rule = `:global .${placement}.t-notify-exit`;
+  const isY = placement && ['top', 'bottom'].includes(placement);
+  let percentage = '';
+
+  switch (placement) {
+    case 'top':
+    case 'left':
+      percentage = '-100%';
+      break;
+    case 'bottom':
+    case 'right':
+      percentage = '100%';
+      break;
+    default:
+      percentage = '100%';
+      break;
+  }
+
+  return {
+   [rule]: {
+      transform: `translate${isY ? 'Y' : 'X'}(${percentage})`,
+   }   
+  };
+}
+
+module.exports = () => {
+  const positions = ['top', 'left', 'bottom', 'right'];
+  const result = positions.reduce((ruleset, placement) => ({ ...ruleset, ...getCssTransform(placement) }), {});
+
+  return result;
+};
