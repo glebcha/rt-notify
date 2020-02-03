e68e666f46c4bedc0138af36da1315f10aef2e34
Gleb Popov <glebcha@gmail.com>
2020-02-03 15:10:56 +0700

    chore: use postcss with plugins

---
+++ postcss.config.js
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    'postcss-global-import': {
+      globalizeKeyframes: true
+    },
+    'postcss-mixins': {
+      mixinsFiles: path.join(__dirname, 'src', 'styles', 'mixins', '!(*.spec.js)'),
+    },
+    'postcss-preset-env': {
+      stage: 3,
+      features: {
+        'nesting-rules': true
+      }
+    }
+  }
+};
