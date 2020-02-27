bfd2081a135c662f2ee8c9db995384858fae197a
Gleb Popov <glebcha@gmail.com>
2020-02-28 00:59:53 +0700

    chore: prettified storybook story

--- docs/notifications.stories.tsx
+++ docs/notifications.stories.tsx
@@ -1,44 +1,46 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { withKnobs, button, select, boolean, number } from '@storybook/addon-knobs'
-import { Notifications, eventEmitter } from '../src/components/Notifications'
-import { Animation, Placement, NotificationProps } from '../src/types'
-
-const Duplicate = (props: { show: boolean }) => props.show &&  
-  <Notifications 
-    notifications={defaultNotifications} 
-    duplicatePlaceholder={<h3 style={{textAlign: 'center'}}>Duplicate Notifications Container</h3>}
-  />
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, button, select, boolean, number } from '@storybook/addon-knobs';
+import { Notifications, eventEmitter } from '../src/components/Notifications';
+import { Animation, Placement, NotificationProps } from '../src/types';
 
 const defaultNotifications: Array<NotificationProps> = [
   { id: 1, type: 'success', content: 'Success Notification', width: '350px', timeout: null },
   { id: 2, type: 'error', content: 'Error Notification', width: '350px', timeout: null },
-]
+];
 
-const placementLabel = 'Placement'
+const Duplicate = (props: { show: boolean }): JSX.Element => props.show &&  
+  <Notifications 
+    notifications={defaultNotifications} 
+    duplicatePlaceholder={<h3 style={{textAlign: 'center'}}>Duplicate Notifications Container</h3>}
+  />;
+
+const placementLabel = 'Placement';
 const placementOptions = {
   top: 'top',
   bottom: 'bottom',
   left: 'left',
   right: 'right',
-}
-const defaultPlacementValue = 'right'
+};
+const defaultPlacementValue = 'right';
 
-const animationLabel = 'Animation type'
+const animationLabel = 'Animation type';
 const animationOptions = {
   fade: 'fade',
   bounce: 'bounce',
   zoom: 'zoom',
-}
-const defaultAnimationValue = 'fade'
+};
+const defaultAnimationValue = 'fade';
 
-const handler = (index: number) => {
+const { log } = console;
+
+const handler = (index: number): void => {
   const types: Array<NotificationProps> = [
     { 
       type: 'waiting', 
       content: 'Waiting Notification New', 
       width: '350px', 
-      onClose: () => console.log('CLOSED WAITING NOTIFICATION') 
+      onClose: (): void => log('CLOSED WAITING NOTIFICATION') 
     },
     { 
       type: 'success', 
@@ -50,13 +52,13 @@ const handler = (index: number) => {
       width: '350px', 
       content: 'Error Notification New' 
     },
-  ]
+  ];
 
-  eventEmitter.emit('add', types[index])
-}
+  eventEmitter.emit('add', types[index]);
+};
 
-const stories = storiesOf('Notifications', module)
-stories.addDecorator(withKnobs)
+const stories = storiesOf('Notifications', module);
+stories.addDecorator(withKnobs);
 
 stories.add('default', () => {
   return (<React.Fragment>
@@ -71,4 +73,4 @@ stories.add('default', () => {
     {button('Add Warning', handler.bind(null, 2))}
     <Duplicate show={boolean('Duplicate Notifications', false)} />
   </React.Fragment>);
-})
+});
