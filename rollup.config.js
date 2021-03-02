8c72b44bf533c0f341da216afd0994f0ee0f7f6f
Gleb Popov <glebcha@gmail.com>
2021-03-03 01:39:50 +0700

    chore(build): get rid of babel, included all external dependencies except react, build umd brwoser module

--- rollup.config.js
+++ rollup.config.js
@@ -5,8 +5,18 @@ import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss-modules';
 import nodeResolve from '@rollup/plugin-node-resolve';
+import externals from 'rollup-plugin-node-externals';
 import { terser } from 'rollup-plugin-terser';
 import { sizeSnapshot } from "rollup-plugin-size-snapshot";
+
+const globals = {
+  react: 'React',
+  clsx: 'clsx',
+  'react-dom': 'ReactDOM',
+  'ramda/src/equals': 'equals', 
+  'ramda/src/findIndex': 'findIndex',
+  'react-transition-group': 'reactTransitionGroup',
+};
 
 /* initialize CSS files because of a catch-22 situation:
    https://github.com/rollup/rollup/issues/1404 */
@@ -16,40 +26,57 @@ glob.sync('src/**/*.css').forEach(css => {  // Use forEach because https://githu
 		fs.writeFileSync(definition, 'const mod: { [cls: string]: string }\nexport default mod\n');
 });
 
-const externalRegExp = /^(?:[./\\]|(?:[a-zA-Z]:)).*$/;
-
 export default {
-  input: './src/index.tsx',
+  input: './src/index.ts',
   output: [
     {
-      file: 'build/index.cjs.js',
-      format: 'cjs',
+      name: 'rtNotify',
+      file: 'build/index.umd.js',
+      format: 'umd',
       sourcemap: true,
+      globals,
     },
     {
       file: 'build/index.esm.js',
       format: 'es',
       sourcemap: true,
-    },
+    }
   ],
-  external: id => !externalRegExp.test(id),
   plugins: [
     replace({
+      preventAssignment: true,
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
+    externals({
+      deps: true,
+      exclude: [
+        'clsx', 
+        'ramda',
+        'react-transition-group',
+      ],      
+    }),
     nodeResolve({
+      browser: true,
       extensions: ['.js', '.jsx', '.ts', '.tsx']
     }),
     postcss({
-			// writeDefinitions: true,
+      // writeDefinitions: true,
+      extract: true,
+      minimize: true,
       modules: {
         generateScopedName: '[name]__[local]___[hash:base64:5]',
       },
     }),
     ts({
-      transpiler: 'babel',
+      browserslist: false,
+      tsconfig: "tsconfig.json",
+      exclude: ['node_modules/**']
     }),
-    commonjs(),
+    commonjs({
+      ignoreGlobal: false,
+      sourceMap: false,
+      include: '**/node_modules/**',
+    }),
     sizeSnapshot(),
     terser(),
   ],
